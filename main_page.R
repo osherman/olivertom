51c16429266326f275cd6f2d62cae0544a9aaeef
osherman <oschuer@gmail.com>
2022-11-02 01:34:24 +0100

    base url change

--- main_page.R
+++ main_page.R
@@ -10,10 +10,11 @@ blogdown::check_site()
 blogdown:::create_index()
 blogdown::config_Rprofile()
 blogdown::config_netlify()
-
+blogdown::config_Rprofile()
 
 ### to undraft ## 
 # blogdown::edit_draft(c(
 #   "content/privacy.md",
 #   "content/terms.md"
 # ))
+rstudioapi::navigateToFile("netlify.toml") 
